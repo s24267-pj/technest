06c5f6476ec7440de3b59cfb8fbe9b99b006ae7c
Damian Ligeza <s24267@pjwstk.edu.pl>
2024-06-03 20:23:03 +0200

    Fix: import "../styles/ProductList.css";

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./styles/ProductList.css";
+import "../styles/ProductList.css";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
